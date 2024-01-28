ba91c171de05f154a5632f158adb69f9badd63e4
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-28 10:54:39 -0300

    Create routes register, authenticate and details

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,7 +1,19 @@
+import { FastifyReply } from "fastify";
+import { FastifyRequest } from "fastify";
 import { FastifyInstance } from "fastify";
+import { RegisterGymUserController } from "../ controllers/gym-user/register";
+import { AuthenticateGymUserController } from "../ controllers/gym-user/authenticate";
 
 export async function appRoutes(app: FastifyInstance){
-  app.get('/', async () => {
-    return { hello : true}
+  app.post('/sessions', async (req: FastifyRequest, reply:FastifyReply) => {
+    return new RegisterGymUserController().handle(req, reply)
+  })
+
+  app.post('/users', async (req: FastifyRequest, reply:FastifyReply) => {
+    return new AuthenticateGymUserController().handle(req, reply)
+  })
+
+  app.post('/details', async (req: FastifyRequest, reply:FastifyReply) => {
+    return new RegisterGymUserController().handle(req, reply)
   })
 }
